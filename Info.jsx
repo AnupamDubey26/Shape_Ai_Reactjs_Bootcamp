d5b96f49b9e23e0a31b0901d6cd3f815af4441b6
Anupam Dubey <82710781+AnupamDubey26@users.noreply.github.com>
2021-06-27 21:25:33 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> CS-GO</h1>
+      <p>
+        Counter-Strike: Global Offensive (CS:GO) is a multiplayer first-person
+        shooter developed by Valve and Hidden Path Entertainment. It is the
+        fourth game in the Counter-Strike series. Developed for over two years,
+        Global Offensive was released for Windows, macOS, Xbox 360, and
+        PlayStation 3 in August 2012, and for Linux in 2014. Valve still
+        regularly updates the game, both with smaller balancing patches and
+        larger content additions.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
